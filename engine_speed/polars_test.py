3c954db44c37baf9e0a9e6a66638d99ca359e34b
lacatt <lavalachlan@gmail.com>
2025-06-19 17:25:51 +1000

    lazy frame vs df polars

--- engine_speed/polars_test.py
+++ engine_speed/polars_test.py
@@ -19,3 +19,27 @@ dummy_pl.get_column("A")
 
 dummy_pl[:10, ["A", "B"]]
 dummy_pl.limit(10).select(["A", "B"])
+
+# filter alltaxi.parquet by pickup_at > '2019-06-30'
+alltaxi_df = pl.read_parquet('alltaxi.parquet')
+alltaxi_df_filtered = alltaxi_df.filter(
+    pl.col("pickup_at").dt.date() > pl.datetime(2019, 6, 30)
+    ).with_columns(
+        ["vendor_id","pickup_at", "total_amount"]
+    ).limit(10)
+print(alltaxi_df)
+
+alltaxi_lf = pl.scan_parquet('alltaxi.parquet')
+alltaxi_lf_filtered = alltaxi_lf.filter(
+    pl.col("pickup_at").dt.date() > pl.datetime(2019, 6, 30)
+    ).with_columns(
+        ["vendor_id","pickup_at", "total_amount"]
+    ).limit(10)
+# print(filtered_scan.show_graph())
+print(alltaxi_lf_filtered.collect())
+
+alltaxi_pd = pd.read_parquet('alltaxi.parquet')
+alltaxi_pd_filtered = alltaxi_pd[
+    alltaxi_pd["pickup_at"] > pd.Timestamp("2019-06-30")
+][["vendor_id", "pickup_at", "total_amount"]].head(10)
+print(alltaxi_pd_filtered)
